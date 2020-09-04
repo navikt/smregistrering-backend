6847f8141a6365077290d4fe12ce047c4f8c33b8
Mathias Hellevang <mathias.hellevang@gmail.com>
2020-09-04 14:20:11 +0200

    Setter jouralførende enhet til å være 9999 frem til vi har noe bedre

--- src/main/kotlin/no/nav/syfo/client/DokArkivClient.kt
+++ src/main/kotlin/no/nav/syfo/client/DokArkivClient.kt
@@ -99,7 +99,7 @@ class DokArkivClient(
             val oidcToken = oidcClient.oidcToken()
             header("Authorization", "Bearer ${oidcToken.access_token}")
             header("Nav-Callid", msgId)
-            body = FerdigstillJournal(veileder.veilederIdent) // TODO: journalfoerendeEnhet er vel ikke det samme som en ident?
+            body = FerdigstillJournal("9999") // TODO: journalfoerendeEnhet må fylles ut når vi har den
         }.execute()
         if (httpResponse.status == HttpStatusCode.InternalServerError) {
             log.error("Dokakriv svarte med feilmelding ved ferdigstilling av journalpost for msgId {}, {}", msgId, fields(loggingMeta))
