c9aa8bb33811baa959631c4179a698d7d3df194a
andreasnilsen <danduras@gmail.com>
2020-10-01 12:52:12 +0200

    Finn aktiv eller inaktiv samhandlerpraksis

--- src/main/kotlin/no/nav/syfo/client/KuhrSarClient.kt
+++ src/main/kotlin/no/nav/syfo/client/KuhrSarClient.kt
@@ -7,13 +7,10 @@ import io.ktor.client.request.parameter
 import io.ktor.http.ContentType
 import io.ktor.util.KtorExperimentalAPI
 import kotlin.math.max
-import net.logstash.logback.argument.StructuredArguments.fields
-import net.logstash.logback.argument.StructuredArguments.keyValue
-import no.nav.syfo.log
 import no.nav.syfo.model.Samhandler
 import no.nav.syfo.model.SamhandlerPeriode
+import no.nav.syfo.model.SamhandlerPraksis
 import no.nav.syfo.model.SamhandlerPraksisMatch
-import no.nav.syfo.model.SamhandlerPraksisType
 import no.nav.syfo.util.LoggingMeta
 import org.apache.commons.text.similarity.LevenshteinDistance
 
@@ -41,72 +38,47 @@ fun List<SamhandlerPeriode>.formaterPerioder() = joinToString(",", "periode(", "
 }
 
 fun List<Samhandler>.formaterPraksis() = flatMap { it.samh_praksis }
-    .joinToString(",", "praksis(", ") ") { praksis ->
-        "${praksis.navn}: ${praksis.samh_praksis_status_kode} ${praksis.samh_praksis_periode.formaterPerioder()}"
-    }
+        .joinToString(",", "praksis(", ") ") { praksis ->
+            "${praksis.navn}: ${praksis.samh_praksis_status_kode} ${praksis.samh_praksis_periode.formaterPerioder()}"
+        }
 
 fun findBestSamhandlerPraksis(
     samhandlere: List<Samhandler>,
     loggingMeta: LoggingMeta
 ): SamhandlerPraksisMatch? {
-    val aktiveSamhandlere = samhandlere.flatMap { it.samh_praksis }
-        .filter { praksis -> praksis.samh_praksis_status_kode == "aktiv" }
-
-    if (aktiveSamhandlere.isEmpty()) {
-        log.info(
-            "Fant ingen aktive samhandlere. {}  Meta: {}, {} ",
-            keyValue("praksis Informasjo", samhandlere.formaterPraksis()),
-            keyValue("antall praksiser", samhandlere.size),
-            fields(loggingMeta)
-        )
-    }
-
-    val aktiveSamhandlereMedNavn = samhandlere.flatMap { it.samh_praksis }
-        .filter { !it.navn.isNullOrEmpty() }
-
-    if (aktiveSamhandlereMedNavn.isNullOrEmpty() && !aktiveSamhandlere.isNullOrEmpty()) {
-        val samhandlerFALEOrFALO = aktiveSamhandlere.find {
-            it.samh_praksis_type_kode == SamhandlerPraksisType.FASTLEGE.kodeVerdi ||
-                    it.samh_praksis_type_kode == SamhandlerPraksisType.FASTLONNET.kodeVerdi
-        }
-        if (samhandlerFALEOrFALO != null) {
-            return SamhandlerPraksisMatch(samhandlerFALEOrFALO, 999.0)
-        }
-        if (samhandlere.firstOrNull()?.samh_praksis != null &&
-            samhandlere.firstOrNull()?.samh_praksis?.firstOrNull() != null
-        ) {
-            val firstSamhnalderPraksis = samhandlere.firstOrNull()?.samh_praksis?.firstOrNull()
-            if (firstSamhnalderPraksis != null && firstSamhnalderPraksis.tss_ident.isNotEmpty()) {
-                log.info(
-                    "Siste utvei med tss matching ble samhandler praksis: " +
-                            "Orgnumer: ${firstSamhnalderPraksis.org_id} " +
-                            "Navn: ${firstSamhnalderPraksis.navn} " +
-                            "Tssid: ${firstSamhnalderPraksis.tss_ident} " +
-                            "Adresselinje1: ${firstSamhnalderPraksis.arbeids_adresse_linje_1} " +
-                            "Samhandler praksis type: ${firstSamhnalderPraksis.samh_praksis_type_kode} " +
-                            "Samhandlers hpr nummer: ${samhandlere.firstOrNull()?.samh_ident?.find { it.ident_type_kode == "HPR" }?.ident} " +
-                            "{}", fields(loggingMeta)
-                )
-                return SamhandlerPraksisMatch(firstSamhnalderPraksis, 999.0)
-            }
+    return getAktivOrInaktivSamhandlerPraksis(samhandlere).let {
+        when (it) {
+            null -> null
+            else -> SamhandlerPraksisMatch(it, 100.0)
         }
     }
+}
 
-    return null
+private fun getAktivOrInaktivSamhandlerPraksis(samhandlere: List<Samhandler>): SamhandlerPraksis? {
+    val activSamhandlerPraksis = samhandlere.flatMap { it.samh_praksis }.groupBy { it.samh_praksis_status_kode }
+    return activSamhandlerPraksis["aktiv"]?.firstOrNull() ?: activSamhandlerPraksis["inaktiv"]?.firstOrNull()
+}
+
+fun isInactiv(it: SamhandlerPraksis): Boolean {
+    return it.samh_praksis_status_kode == "inaktiv"
+}
+
+private fun isAktiv(it: SamhandlerPraksis): Boolean {
+    return it.samh_praksis_status_kode == "aktiv"
 }
 
 fun samhandlerMatchingPaaOrganisjonsNavn(samhandlere: List<Samhandler>, orgName: String): SamhandlerPraksisMatch? {
     val inaktiveSamhandlereMedNavn = samhandlere.flatMap { it.samh_praksis }
-        .filter { samhandlerPraksis -> samhandlerPraksis.samh_praksis_status_kode == "inaktiv" }
-        .filter { samhandlerPraksis -> !samhandlerPraksis.navn.isNullOrEmpty() }
+            .filter { samhandlerPraksis -> samhandlerPraksis.samh_praksis_status_kode == "inaktiv" }
+            .filter { samhandlerPraksis -> !samhandlerPraksis.navn.isNullOrEmpty() }
     return if (!inaktiveSamhandlereMedNavn.isNullOrEmpty()) {
         inaktiveSamhandlereMedNavn
-            .map { samhandlerPraksis ->
-                SamhandlerPraksisMatch(
-                    samhandlerPraksis,
-                    calculatePercentageStringMatch(samhandlerPraksis.navn?.toLowerCase(), orgName.toLowerCase()) * 100
-                )
-            }.maxBy { it.percentageMatch }
+                .map { samhandlerPraksis ->
+                    SamhandlerPraksisMatch(
+                            samhandlerPraksis,
+                            calculatePercentageStringMatch(samhandlerPraksis.navn?.toLowerCase(), orgName.toLowerCase()) * 100
+                    )
+                }.maxBy { it.percentageMatch }
     } else {
         null
     }
--- src/test/kotlin/no/nav/syfo/client/KuhrSarClientTest.kt
+++ src/test/kotlin/no/nav/syfo/client/KuhrSarClientTest.kt
@@ -5,18 +5,24 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.mockk.every
+import io.mockk.mockk
 import no.nav.syfo.model.Samhandler
+import no.nav.syfo.model.SamhandlerPraksis
+import no.nav.syfo.util.LoggingMeta
 import org.amshove.kluent.shouldEqual
 import org.junit.Test
 
-internal class KuhrSarClientTest {
+class KuhrSarClientTest {
     val objectMapper = ObjectMapper()
         .registerKotlinModule()
         .registerModule(JavaTimeModule())
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
+    val loggingMeta = mockk<LoggingMeta>(relaxed = true)
+    val samhandlerPraksis = mockk<SamhandlerPraksis>(relaxed = true)
     @Test
-    internal fun `Finner en samhandler når det bare er inaktivte samhandlere`() {
+    fun `Finner en samhandler når det bare er inaktivte samhandlere`() {
         val samhandlerMedNavn: List<Samhandler> = objectMapper.readValue(
             KuhrSarClientTest::class.java.getResourceAsStream("/kuhr_sahr_response_inaktive.json").readBytes()
                 .toString(Charsets.UTF_8)
@@ -25,5 +31,64 @@ internal class KuhrSarClientTest {
         val match = samhandlerMatchingPaaOrganisjonsNavn(samhandlerMedNavn, "Testlegesenteret")
 
         match?.samhandlerPraksis?.navn shouldEqual "Testlegesenteret - org nr"
+    }
+
+    @Test
+    fun findSamhandlerKiropraktorActive() {
+        every { samhandlerPraksis.tss_ident } returns "123456789101112"
+        every { samhandlerPraksis.samh_praksis_status_kode } returns "aktiv"
+        val samhandlere = listOf(Samhandler(
+                "1",
+                "samhandlernavn",
+                "KI",
+                "",
+                "",
+                "", "", "", "", null, emptyList(), listOf(samhandlerPraksis)
+        ))
+        val samhandlerPraksisMatch = findBestSamhandlerPraksis(samhandlere, loggingMeta)
+        samhandlerPraksisMatch?.samhandlerPraksis?.tss_ident shouldEqual "123456789101112"
+    }
+
+    @Test
+    fun findSamhandlerKiropraktorInaktivOgActive() {
+        val samhandlerPraksis2 = mockk<SamhandlerPraksis>(relaxed = true)
+        every { samhandlerPraksis2.tss_ident } returns "2"
+        every { samhandlerPraksis2.samh_praksis_status_kode } returns "inaktiv"
+        every { samhandlerPraksis.tss_ident } returns "123456789101112"
+        every { samhandlerPraksis.samh_praksis_status_kode } returns "aktiv"
+        val samhandlere = getSamhandler("KI", samhandlerPraksis2, samhandlerPraksis)
+        val samhandlerPraksisMatch = findBestSamhandlerPraksis(samhandlere, loggingMeta)
+        samhandlerPraksisMatch?.samhandlerPraksis?.tss_ident shouldEqual "123456789101112"
+    }
+
+    private fun getSamhandler(type: String, vararg samhandlerPraksis: SamhandlerPraksis): MutableList<Samhandler> {
+        val samhandlere = mutableListOf(Samhandler(
+                "1",
+                "samhandlernavn",
+                type,
+                "",
+                "",
+                "", "", "", "", null, emptyList(), samhandlerPraksis.toList())
+        )
+        return samhandlere
+    }
+
+    @Test
+    fun findFirstActiveSamhandlerPraksis() {
+        val samhandlerPraksis2 = mockk<SamhandlerPraksis>(relaxed = true)
+        every { samhandlerPraksis2.tss_ident } returns "2"
+        every { samhandlerPraksis2.samh_praksis_status_kode } returns "aktiv"
+        val samhandlere = getSamhandler("LE", samhandlerPraksis2)
+        val samhandlerPraksisMatch = findBestSamhandlerPraksis(samhandlere, loggingMeta)
+        samhandlerPraksisMatch?.samhandlerPraksis?.tss_ident shouldEqual "2"
+    }
+
+    @Test
+    fun findInactiveSamhandlerPraksis() {
+        every { samhandlerPraksis.tss_ident } returns "1"
+        every { samhandlerPraksis.samh_praksis_status_kode } returns "inaktiv"
+        val samhandlere = getSamhandler("ANY", samhandlerPraksis)
+        val samhandlerPraksisMatch = findBestSamhandlerPraksis(samhandlere, loggingMeta)
+        samhandlerPraksisMatch?.samhandlerPraksis?.tss_ident shouldEqual "1"
     }
 }
