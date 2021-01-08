4ae026f064ca68819a490c917634a8a27675349d
Tia Firing <tia.firing@nav.no>
2021-01-08 14:18:47 +0100

    tillater ikke overlappende perioder eller ulovlige kombinasjoner med reisetilskudd

--- src/main/kotlin/no/nav/syfo/persistering/api/ValidationRules.kt
+++ src/main/kotlin/no/nav/syfo/persistering/api/ValidationRules.kt
@@ -1,5 +1,7 @@
 package no.nav.syfo.persistering.api
 
+import java.time.LocalDate
+import no.nav.syfo.model.Periode
 import no.nav.syfo.model.RuleInfo
 import no.nav.syfo.model.SmRegistreringManuell
 import no.nav.syfo.model.Status
@@ -9,15 +11,15 @@ fun validate(smRegistreringManuell: SmRegistreringManuell) {
 
     if (smRegistreringManuell.prognose?.erIArbeid != null && smRegistreringManuell.prognose.erIkkeIArbeid != null) {
         val validationResult = ValidationResult(
-                status = Status.MANUAL_PROCESSING,
-                ruleHits = listOf(
-                        RuleInfo(
-                                ruleName = "erIArbeidValidation",
-                                messageForSender = "Sykmeldingen kan ikke ha både 5.2 og 5.3 fylt ut samtidig",
-                                messageForUser = "Sykmelder har gjort en feil i utfyllingen av sykmeldingen.",
-                                ruleStatus = Status.MANUAL_PROCESSING
-                        )
+            status = Status.MANUAL_PROCESSING,
+            ruleHits = listOf(
+                RuleInfo(
+                    ruleName = "erIArbeidValidation",
+                    messageForSender = "Sykmeldingen kan ikke ha både 5.2 og 5.3 fylt ut samtidig",
+                    messageForUser = "Sykmelder har gjort en feil i utfyllingen av sykmeldingen.",
+                    ruleStatus = Status.MANUAL_PROCESSING
                 )
+            )
         )
         throw ValidationException(validationResult)
     } else if (smRegistreringManuell.perioder.isEmpty()) {
@@ -33,5 +35,52 @@ fun validate(smRegistreringManuell: SmRegistreringManuell) {
             )
         )
         throw ValidationException(validationResult)
+    } else if (harOverlappendePerioder(smRegistreringManuell.perioder)) {
+        val validationResult = ValidationResult(
+            status = Status.MANUAL_PROCESSING,
+            ruleHits = listOf(
+                RuleInfo(
+                    ruleName = "overlappendePeriodeValidation",
+                    messageForSender = "Sykmeldingen har overlappende perioder",
+                    messageForUser = "Sykmelder har gjort en feil i utfyllingen av sykmeldingen.",
+                    ruleStatus = Status.MANUAL_PROCESSING
+                )
+            )
+        )
+        throw ValidationException(validationResult)
+    } else if (harUlovligKombinasjonMedReisetilskudd(smRegistreringManuell.perioder)) {
+        val validationResult = ValidationResult(
+            status = Status.MANUAL_PROCESSING,
+            ruleHits = listOf(
+                RuleInfo(
+                    ruleName = "reisetilskuddValidation",
+                    messageForSender = "Sykmeldingen inneholder periode som kombinerer reisetilskudd med annen sykmeldingstype",
+                    messageForUser = "Sykmelder har gjort en feil i utfyllingen av sykmeldingen.",
+                    ruleStatus = Status.MANUAL_PROCESSING
+                )
+            )
+        )
+        throw ValidationException(validationResult)
     }
 }
+
+fun harOverlappendePerioder(perioder: List<Periode>): Boolean {
+    return perioder.any { periodA ->
+        perioder
+            .filter { periodB -> periodB != periodA }
+            .any { periodB ->
+                periodA.fom in periodB.range() || periodA.tom in periodB.range()
+            }
+    }
+}
+
+fun harUlovligKombinasjonMedReisetilskudd(perioder: List<Periode>): Boolean {
+    perioder.forEach {
+        if (it.reisetilskudd && (it.aktivitetIkkeMulig != null || it.gradert != null || it.avventendeInnspillTilArbeidsgiver != null || it.behandlingsdager != null)) {
+            return true
+        }
+    }
+    return false
+}
+
+fun Periode.range(): ClosedRange<LocalDate> = fom.rangeTo(tom)
