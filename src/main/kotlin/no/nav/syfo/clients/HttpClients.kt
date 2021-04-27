0072c2939d3f98394f653d015a26ab24ea58db1e
Mathias Hellevang <mathias.hellevang@gmail.com>
2021-04-27 09:08:31 +0200

    Lint...

--- src/main/kotlin/no/nav/syfo/clients/HttpClients.kt
+++ src/main/kotlin/no/nav/syfo/clients/HttpClients.kt
@@ -1,6 +1,5 @@
 package no.nav.syfo.clients
 
-import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
