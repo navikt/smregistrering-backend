78318b4e2e19b826783b670773c26a4cff7299bc
Mathias Hellevang <mathias.hellevang@gmail.com>
2021-04-27 10:09:44 +0200

    Carry on and debug

--- src/main/kotlin/no/nav/syfo/model/Oppgave.kt
+++ src/main/kotlin/no/nav/syfo/model/Oppgave.kt
@@ -3,10 +3,10 @@ package no.nav.syfo.model
 import java.time.LocalDate
 
 data class Oppgave(
-    val id: Int? = null,
+    val id: Int?,
     val versjon: Int? = null,
     val tildeltEnhetsnr: String? = null,
-    val opprettetAvEnhetsnr: String? = null,
+    val opprettetAvEnhetsnr: String?,
     val aktoerId: String? = null,
     val journalpostId: String? = null,
     val behandlesAvApplikasjon: String? = null,
