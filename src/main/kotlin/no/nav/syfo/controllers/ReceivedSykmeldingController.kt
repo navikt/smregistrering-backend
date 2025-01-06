7091332d2fffa9e1e3916bf5f208f5b613c8ddce
Helene Arnesen <helene.arnesen@nav.no>
2025-01-06 11:38:24 +0100

    Reapply "fix: catch runtimeexception"
    This reverts commit c3574e2e93b02af1fe0d0ad91a44c1ac2e01905e.

--- src/main/kotlin/no/nav/syfo/controllers/ReceivedSykmeldingController.kt
+++ src/main/kotlin/no/nav/syfo/controllers/ReceivedSykmeldingController.kt
@@ -41,14 +41,18 @@ class ReceivedSykmeldingController(
                     papirSmRegistering.sykmeldingId,
                 )
             } else {
-                val oppgave = oppgaveService.upsertOppgave(papirSmRegistering, loggingMeta)
-                database.opprettManuellOppgave(papirSmRegistering, oppgave.id!!)
-                log.info(
-                    "Manuell papirsykmeldingoppgave lagret i databasen, for {}, {}",
-                    StructuredArguments.keyValue("oppgaveId", oppgave.id),
-                    StructuredArguments.fields(loggingMeta),
-                )
-                MESSAGE_STORED_IN_DB_COUNTER.inc()
+                try {
+                    val oppgave = oppgaveService.upsertOppgave(papirSmRegistering, loggingMeta)
+                    database.opprettManuellOppgave(papirSmRegistering, oppgave.id!!)
+                    log.info(
+                        "Manuell papirsykmeldingoppgave lagret i databasen, for {}, {}",
+                        StructuredArguments.keyValue("oppgaveId", oppgave.id),
+                        StructuredArguments.fields(loggingMeta),
+                    )
+                    MESSAGE_STORED_IN_DB_COUNTER.inc()
+                } catch (ex: RuntimeException) {
+                    log.error("caught Runtimeexception {}, {}", papirSmRegistering.sykmeldingId, ex.message, ex)
+                }
             }
         }
     }
